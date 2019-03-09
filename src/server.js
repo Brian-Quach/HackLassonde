c6f21276f789f65c4b50c09366d6e7944d934d46
brian-quach <briiquach@gmail.com>
2019-03-09 13:58:54 -0500

    Mongo connection

--- src/server.js
+++ src/server.js
@@ -9,10 +9,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Database
+const mongoose=require("mongoose");
+const uri = "mongodb+srv://Brian:hacklassonde@hlassonde-x6o9m.gcp.mongodb.net/test?retryWrites=true";
+
 
 // Use apiRoutes
-app.use("/api",apiRoutes);
+app.use("/api",apiRoutes)
+
+mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
 
 app.listen(PORT, function() {
   console.log(`API server now on port ${PORT}!`);
 });
+
+
+

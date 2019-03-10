5e98faee31dd1e482abdffafdd2d086d9bea039f
brian-quach <briiquach@gmail.com>
2019-03-10 09:17:54 -0400

    Still tired

--- src/server.js
+++ src/server.js
@@ -59,7 +59,7 @@ app.use(function (req, res, next){
     next();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log("Started server on port", PORT);
 });

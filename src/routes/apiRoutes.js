c6647f45b0adb21ad7137bd6325f81de1ce5f3ab
Oleksii <nidjun13@gmail.com>
2019-03-09 23:28:20 -0500

    image upload

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,18 +1,17 @@
 //const controller = require("../controllers/apiController");
 const account = require("../controllers/accountController");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const fs = require('fs');
 
 const userAccount = require("../models/Users");
 
+// image upload package
+
+
 
 module.exports = function(app){
 
-    const database = mongoose.connection;
-    database.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    database.once('open', function() {
-        console.log("Database Connection Established!");
-    });
+  
 
     app.get("/test", function(req, res){
         return res.json('hello');
@@ -20,5 +19,6 @@ module.exports = function(app){
 
     app.post("/createAccount", account.createAccount);
     app.post("/login", account.login);
+
 
 };
--- src/server.js
+++ src/server.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const app = express();
 
+
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
+
+
 // NeDB for PoC
 const Datastore = require('nedb');
 
-const mongoose = require('mongoose');
-const mongoDb = "mongodb+srv://Brian:hacklassonde@hlassonde-x6o9m.gcp.mongodb.net/test?retryWrites=true";
 
-mongoose.connect(mongoDb, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+// Multer ================================ upload image
+const multer  = require('multer')
+
+
+ const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'storage')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+const upload = multer({ storage: storage })
+
+app.post('/image', upload.single('image'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send(file)
+  
+})
+
+
+
+// ===============================================
 //const apiRoutes =
 require('./routes/apiRoutes')(app);
 //app.use("/",apiRoutes);
